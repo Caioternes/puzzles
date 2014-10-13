54c1c33002bd5c470b8ab5e7260111692744d2bd
Caioternes <caio.ternes@uol.com.br>
2014-10-13 14:00:27 -0300

    Commit Telefone
    Commit do algoritmo do problema do telefone

--- scr/telefone.php
+++ scr/telefone.php
@@ -2,7 +2,49 @@
 
 class Telefone
 {
-
+	public $desc;
+	
+	function __construct($lerDesc)
+	{
+		$this->desc = $lerDesc;
+	}
+	
+	public function numeroTelefone(){
+		$descExplode = strtolower($this->desc);
+		$cont = strlen($descExplode);//conta quantos caracteres tem a String
+		
+		$i = 0;
+		$numeroTel = "";
+		while($i < $cont){
+		
+			if ($descExplode[$i] == "1"){
+				$numeroTel = $numeroTel."1";
+			}elseif($descExplode[$i] == "0"){
+				$numeroTel = $numeroTel."0";
+			}elseif($descExplode[$i] == "-"){
+				$numeroTel = $numeroTel."-";
+			}elseif($descExplode[$i] == "a" or $descExplode[$i] == "b" or $descExplode[$i] == "c"){
+				$numeroTel = $numeroTel."2";
+			}elseif($descExplode[$i] == "d" or $descExplode[$i] == "e" or $descExplode[$i] == "f"){
+				$numeroTel = $numeroTel."3";
+			}elseif($descExplode[$i] == "g" or $descExplode[$i] == "h" or $descExplode[$i] == "i"){
+				$numeroTel = $numeroTel."4";
+			}elseif($descExplode[$i] == "j" or $descExplode[$i] == "k" or $descExplode[$i] == "l"){
+				$numeroTel = $numeroTel."5";
+			}elseif($descExplode[$i] == "m" or $descExplode[$i] == "n" or $descExplode[$i] == "o"){
+				$numeroTel = $numeroTel."6";
+			}elseif($descExplode[$i] == "p" or $descExplode[$i] == "q" or $descExplode[$i] == "r"  or $descExplode[$i] == "s"){
+				$numeroTel = $numeroTel."7";
+			}elseif($descExplode[$i] == "t" or $descExplode[$i] == "u" or $descExplode[$i] == "v"){
+				$numeroTel = $numeroTel."8";
+			}elseif($descExplode[$i] == "w" or $descExplode[$i] == "x" or $descExplode[$i] == "y"  or $descExplode[$i] == "z"){
+				$numeroTel = $numeroTel."9";
+			}
+		
+			$i++;
+		}
+		return $numeroTel;
+	}
 }
 
 ?>
--- tests/telefone_test.php
+++ tests/telefone_test.php
@@ -3,7 +3,95 @@ require_once "scr/telefone.php";
 
 class TelefoneTest extends PHPUnit_Framework_TestCase
 {
-
+	public function testSemLetras()
+    {
+        $telefone = new Telefone("11");
+        $this->assertEquals("11", $telefone->numeroTelefone());
+    }
+	
+	public function testCom0()
+    {
+        $telefone = new Telefone("00");
+        $this->assertEquals("00", $telefone->numeroTelefone());
+    }
+	
+	public function testSoHifen()
+    {
+        $telefone = new Telefone("--");
+        $this->assertEquals("--", $telefone->numeroTelefone());
+    }
+	
+	public function testCom2()
+    {
+        $telefone = new Telefone("ABC");
+        $this->assertEquals("222", $telefone->numeroTelefone());
+    }
+	
+	public function testCom3()
+    {
+        $telefone = new Telefone("DEF");
+        $this->assertEquals("333", $telefone->numeroTelefone());
+    }
+	
+	public function testCom4()
+    {
+        $telefone = new Telefone("GHI");
+        $this->assertEquals("444", $telefone->numeroTelefone());
+    }
+	
+	public function testCom5()
+    {
+        $telefone = new Telefone("JKL");
+        $this->assertEquals("555", $telefone->numeroTelefone());
+    }
+	
+	public function testCom6()
+    {
+        $telefone = new Telefone("MNO");
+        $this->assertEquals("666", $telefone->numeroTelefone());
+    }
+	
+	public function testCom7()
+    {
+        $telefone = new Telefone("PQRS");
+        $this->assertEquals("7777", $telefone->numeroTelefone());
+    }
+	
+	public function testCom8()
+    {
+        $telefone = new Telefone("TUV");
+        $this->assertEquals("888", $telefone->numeroTelefone());
+    }
+	
+	public function testCom9()
+    {
+        $telefone = new Telefone("WXYZ");
+        $this->assertEquals("9999", $telefone->numeroTelefone());
+    }
+	
+	public function testSomenteLetras()
+    {
+        $telefone = new Telefone("CaioTernesWessling");
+        $this->assertEquals("224683763793775464", $telefone->numeroTelefone());
+    }
+	
+	public function testCom1()
+    {
+        $telefone = new Telefone("1CaioTernesWessling");
+        $this->assertEquals("1224683763793775464", $telefone->numeroTelefone());
+    }
+	
+	public function testComHifen()
+    {
+        $telefone = new Telefone("1Caio-Ternes-Wessling");
+        $this->assertEquals("12246-837637-93775464", $telefone->numeroTelefone());
+    }
+	
+	public function testCom0noFinal()
+    {
+        $telefone = new Telefone("1Caio-Ternes-Wessling0");
+        $this->assertEquals("12246-837637-937754640", $telefone->numeroTelefone());
+    }
 }
 
 ?>
