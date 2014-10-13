8b74cf060912958be6c674e9f50f3d4f46244cef
Caioternes <caio.ternes@uol.com.br>
2014-10-13 13:12:41 -0300

    Commit Autor
    Commit do algoritmo do problema autor ja concluido

--- scr/autor.php
+++ scr/autor.php
@@ -2,9 +2,59 @@
 
 class Autor
 {
-	public function formatar($nnomes, $nome)
-	{
+ 
+	public $nome;
 	
+	function __construct($lerNome)
+	{
+		$this->nome = $lerNome;
+	}
+	
+	public function formatar()
+	{
+		
+		$nomeExplode = explode(" ", $this->nome);
+		
+		$nnomes = count($nomeExplode);
+		
+		$i = 1; //primeiro nome não entra no while
+		$nentrou = 0;
+		while($i < $nnomes){
+			
+			if($nentrou > 0){
+				$nomeExplode[$i - $nentrou] = $nomeExplode[$i]; //volta uma posição no array ja que a posição do "da", "de" etc ficou vaga
+			}
+			if (strtolower($nomeExplode[$i]) == "da" or strtolower($nomeExplode[$i]) == "das" or strtolower($nomeExplode[$i]) == "de"
+				or strtolower($nomeExplode[$i]) == "do" or strtolower($nomeExplode[$i]) == "dos" or strtolower($nomeExplode[$i]) == "neto"
+				or strtolower($nomeExplode[$i]) == "filho" or strtolower($nomeExplode[$i]) == "sobrinho" or strtolower($nomeExplode[$i]) == "junior"){
+				$nentrou += 1; //número de vezes que ele precisa recuar no array
+				$nomeExplode[$i - $nentrou] = $nomeExplode[$i - $nentrou]." ".$nomeExplode[$i];
+			}
+			
+			$i++;
+		}
+		if($nentrou > 0){
+			$nnomes = $nnomes - $nentrou;// "da", "de" etc deixou de ser separado, por isso diminui pelo número de vezes que precisou recuar
+		}
+		
+		
+		if ($nnomes == 1){
+			return strtoupper($nomeExplode[0]);
+		}else{
+			$i = 0;
+			$nomefinal = "";
+			while ($i < $nnomes){
+				if ($i == ($nnomes - 1)){
+					$nomefinal = strtoupper($nomeExplode[$i]).", ".$nomefinal;
+				}elseif($i == 0){
+					$nomefinal = ucfirst(strtolower($nomeExplode[0]));
+				}else{
+					$nomefinal = $nomefinal." ".ucfirst(strtolower($nomeExplode[$i]));
+				}
+				$i++;
+			}
+			return $nomefinal;
+		}
 	}
 }
 
--- tests/autor_test.php
+++ tests/autor_test.php
@@ -1,5 +1,5 @@
 <?php
-require_once "scr/autores.php";
+require_once "scr/autor.php";
 
 class AutorTest extends PHPUnit_Framework_TestCase
 {
@@ -9,77 +9,77 @@ class AutorTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("GUIMARAES", $autor->formatar());
     }
 
-    /*public function testNomeMaisUmSobrenome()
+    public function testNomeMaisUmSobrenome()
     {
         $autor = new Autor("Joao Silva");
         $this->assertEquals("SILVA, Joao", $autor->formatar());
     }
-
+	
     public function testNomeEmMinusculo()
     {
         $autor = new Autor("joao silva");
         $this->assertEquals("SILVA, Joao", $autor->formatar());
     }
-
+	
     public function testNomeEmMaiusculo()
     {
         $autor = new Autor("JOAO SILVA");
         $this->assertEquals("SILVA, Joao", $autor->formatar());
     }
-
+	
     public function testNomeComDA()
     {
         $autor = new Autor("Joao da Silva");
         $this->assertEquals("SILVA, Joao da", $autor->formatar());
     }
-
+	
     public function testNomeComDE()
     {
         $autor = new Autor("Pedro de Jesus");
         $this->assertEquals("JESUS, Pedro de", $autor->formatar());
     }
-
+	
     public function testNomeComDO()
     {
         $autor = new Autor("Maria do Bonfim");
         $this->assertEquals("BONFIM, Maria do", $autor->formatar());
     }
-
+	
     public function testNomeComDAS()
     {
         $autor = new Autor("Ana das Rosas");
         $this->assertEquals("ROSAS, Ana das", $autor->formatar());
     }
-
+	
     public function testNomeComDOS()
     {
         $autor = new Autor("Mario dos Anjos");
         $this->assertEquals("ANJOS, Mario dos", $autor->formatar());
     }
-
+	
     public function testNomeComNeto()
     {
         $autor = new Autor("Joao da Silva Neto");
         $this->assertEquals("SILVA NETO, Joao da", $autor->formatar());
     }
-
+	
     public function testNomeComFilho()
     {
         $autor = new Autor("Joao da Silva Filho");
         $this->assertEquals("SILVA FILHO, Joao da", $autor->formatar());
     }
-
+	
     public function testNomeComSobrinho()
     {
         $autor = new Autor("Joao da Silva Sobrinho");
         $this->assertEquals("SILVA SOBRINHO, Joao da", $autor->formatar());
     }
-
+	
     public function testNomeComJunior()
     {
         $autor = new Autor("Joao da Silva Junior");
         $this->assertEquals("SILVA JUNIOR, Joao da", $autor->formatar());
-    }*/
+    }
 }
 
 ?>
